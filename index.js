0e03bce320366696e51b253b8441022a425a2a5a
sentidosfamily <134061501+sentidosfamily@users.noreply.github.com>
2025-06-05 14:43:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 // Rutas
