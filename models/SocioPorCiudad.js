c90c7785d24f4dd9a29d15017c9c4aa60519fd9a
sentidosfamily <134061501+sentidosfamily@users.noreply.github.com>
2025-06-09 14:02:41 -0300

    Update SocioPorCiudad.js

--- models/SocioPorCiudad.js
+++ models/SocioPorCiudad.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const SocioPorCiudadSchema = new mongoose.Schema({
   correo: { type: String, required: true, unique: true },
-  cuota: { type: Number, default: 12 }, // Agregar la cuota como campo con valor por defecto 12
+  cuota: { type: Number, default: 12 },
   provincia: { type: String, required: true },
   ciudad: { type: String, required: true },
   socioId: { type: mongoose.Schema.Types.ObjectId, ref: "Socio" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" } // 👈 Nuevo campo
 });
 
 module.exports = mongoose.model("SocioPorCiudad", SocioPorCiudadSchema);
